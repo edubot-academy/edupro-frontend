6410bb3cd9a0a2accababd7249d101e8d6a4ca24
kulpunai180 <jekshenbekova@outlook.com>
2025-10-11 15:02:10 +0600

    add translate

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Test() {
-  return (
-    <div className='flex flex-col justify-center items-center gap-6 bg-[#FFFFFF] border border-gray rounded-xl mx-14 py-10 px-4'>
-      <h3 className='text-[#32215E] font-bold text-4xl text-center'>Пройдите тест бесплатно и <br /> узнайте свой уровень!</h3>
-      <p className='font-normal text-base text-center'>Ответьте на несколько простых вопросов и получите мгновенный результат. Определите свой текущий уровень знаний и поймите, <br /> с чего начать обучение. Тест абсолютно бесплатный и не займет больше 5 минут.</p>
-      <button className='bg-gradient-to-t to-[#7640FF] from-[#472799] w-44 text-[#FFFFFF]'>Пройти тест</button> {/*стиль кнопки временный,поменяем на ui когда оно будет готово */}
-    </div>
-  )
+    const { t } = useTranslation(["home"]);
+
+    return (
+        <div className="flex flex-col justify-center items-center gap-6 bg-[#FFFFFF] border border-gray rounded-xl mx-14 py-10 px-4">
+            <h3 className="text-[#32215E] font-bold text-4xl text-center max-w-xl">
+                {t("home:freeTest.title")}
+            </h3>
+            <p className="font-normal text-base text-center max-w-4xl">
+                {t("home:freeTest.text")}
+            </p>
+            <button className="bg-gradient-to-t to-[#7640FF] from-[#472799] w-44 text-[#FFFFFF] rounded-lg py-2">
+                {t("home:freeTest.button")}
+            </button>
+        </div>
+    );
 }
 
-export default Test
+export default Test;
