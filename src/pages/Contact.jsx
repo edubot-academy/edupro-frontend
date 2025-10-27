9db4f6d2b89ab6696ba4ab63cf2fa88c2339d357
Саадат <saadat@MacBook-Air-Saadat.local>
2025-10-27 18:47:24 +0600

    added test component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 
 import LeadForm from "../components/LeadForm";
+import Test from "../components/Test";
 export default function Contact() {
    
     return (
         <main className="bg-white">
             <LeadForm />
+            <Test />
         </main>
     );
 
